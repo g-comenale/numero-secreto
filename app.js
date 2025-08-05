154122e92dbff2c93db068e9895268f4f25be0b1
g-comenale <gabriel.comenale@outlook.com>
2025-08-05 10:51:53 -0300

    de 50 para 100

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 let listaDeNumerosSorteados = [];
 <<<<<<< HEAD
-let numeroLimite = 50;
+let numeroLimite = 100;
 =======
-let numeroLimite = 1000;
+let numeroLimite = 100;
 >>>>>>> parent of 9b6b2a5 (voltando para 100)
 let numeroSecreto = gerarNumeroAleatorio();
 let tentativas = 1;
@@ -15,7 +15,7 @@ function exibirTextoNaTela(tag, texto) {
 
 function exibirMensagemInicial() {
     exibirTextoNaTela('h1', 'Jogo do número secreto');
-    exibirTextoNaTela('p', 'Escolha um número entre 1 e 10');
+    exibirTextoNaTela('p', 'Escolha um número entre 1 e 100');
 }
 
 exibirMensagemInicial();
